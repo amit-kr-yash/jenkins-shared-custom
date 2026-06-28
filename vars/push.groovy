fcbd64b5e1452aa10e265ee37375a548bf014bab
amit-kr-yash <codewithyash10@gmail.com>
2026-06-28 11:50:02 +0530

    Naming convention error solved in push.groovy file

--- vars/push.groovy
+++ vars/push.groovy
@@ -3,5 +3,5 @@ def call(String Project, String ImageTag, String dockerhubuser){
     withCredentials([usernamePassword(credentialsId: 'docker-hub-cred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
         sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
     }
-    sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
+    sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
