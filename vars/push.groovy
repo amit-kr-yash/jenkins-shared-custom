20321b5b8015f605f7cea7eeedda234cee863535
amit-kr-yash <codewithyash10@gmail.com>
2026-06-28 11:44:13 +0530

    Build-push-deploy

---
+++ vars/push.groovy
@@ -0,0 +1,7 @@
+def call(String Project, String ImageTag, String dockerhubuser){
+    echo "This is pushing image to docker hub"
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-cred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
+        sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+    }
+    sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
+}
