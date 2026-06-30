3f7fc836111f2202b8a4d540b6070fd898049d9f
amit-kr-yash <codewithyash10@gmail.com>
2026-06-30 11:22:44 +0530

    Updating owasp dependency fike

--- vars/owasp_dependency_nvd.groovy
+++ vars/owasp_dependency_nvd.groovy
@@ -1,9 +1,12 @@
 def call() {
     withCredentials([string(credentialsId: 'nvd-api-key', variable: 'NVD_API_KEY')]) {
-        dependencyCheck(
-            additionalArguments: "--scan ./ --nvdApiKey ${NVD_API_KEY}",
-            odcInstallation: 'OWASP'
-        )
+        withEnv(["NVD_API_KEY=${NVD_API_KEY}"]) {
+
+            dependencyCheck(
+                additionalArguments: "--scan ./",
+                odcInstallation: 'OWASP'
+            )
+        }
     }
 
     dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
