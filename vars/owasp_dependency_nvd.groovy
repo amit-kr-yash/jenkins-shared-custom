9e4c66f8e76fd861e65fefa0ee2f52dd1f58a921
amit-kr-yash <codewithyash10@gmail.com>
2026-06-30 10:45:38 +0530

    Added owasp dependency check with nvd key to faster the process

---
+++ vars/owasp_dependency_nvd.groovy
@@ -0,0 +1,10 @@
+def call() {
+    withCredentials([string(credentialsId: 'nvd-api-key', variable: 'NVD_API_KEY')]) {
+        dependencyCheck(
+            additionalArguments: "--scan ./ --nvdApiKey ${NVD_API_KEY}",
+            odcInstallation: 'OWASP'
+        )
+    }
+
+    dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+}
