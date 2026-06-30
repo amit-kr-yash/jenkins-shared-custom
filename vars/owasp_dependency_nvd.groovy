ac233439325f2513e49098544f706c2d7a1a59c7
amit-kr-yash <codewithyash10@gmail.com>
2026-06-30 11:25:39 +0530

    reupadte

--- vars/owasp_dependency_nvd.groovy
+++ vars/owasp_dependency_nvd.groovy
@@ -1,12 +1,10 @@
 def call() {
     withCredentials([string(credentialsId: 'nvd-api-key', variable: 'NVD_API_KEY')]) {
-        withEnv(["NVD_API_KEY=${NVD_API_KEY}"]) {
 
-            dependencyCheck(
-                additionalArguments: "--scan ./",
-                odcInstallation: 'OWASP'
-            )
-        }
+        dependencyCheck(
+            additionalArguments: "-Dnvd.api.key=${NVD_API_KEY} --scan ./",
+            odcInstallation: 'OWASP'
+        )
     }
 
     dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
